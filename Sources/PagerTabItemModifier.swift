74eae4a245818bdc2a42f7b0ee0b2e56a9fa31c8
susieyy <y.sugigami@gmail.com>
2022-01-27 22:42:41 +0900

    Fix zero division

--- Sources/PagerTabItemModifier.swift
+++ Sources/PagerTabItemModifier.swift
@@ -22,7 +22,11 @@ struct PagerTabItemModifier<NavTabView: View>: ViewModifier {
                     let dataStore = self.dataStore
                     DispatchQueue.main.async {
                         let frame = reader.frame(in: .named("PagerViewScrollView"))
-                        index = Int(round(frame.minX / frame.width))
+                        if frame.width.isZero {
+                            index = 0
+                        } else {
+                            index = Int(round(frame.minX / frame.width))
+                        }
                         let tabView = navTabView()
                         let tabViewDelegate = tabView as? PagerTabViewDelegate
                         dataStore.setView(AnyView(tabView), at: index)
